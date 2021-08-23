25aaead997adc4a8bcb7ecc6101011f486b3d307
FrankyNeedles <frankyneedles@gmail.com>
2021-08-23 12:52:46 -0400

    Stage

--- src/game.ts
+++ src/game.ts
@@ -2,17 +2,32 @@
 //  import {spawnGltfX, spawnEntity, spawnBoxX, spawnPlaneX} from './modules/SpawnerFunctions'
 
 //  const hud = new BuilderHUD()
-const defaultbuilding = new Entity('defaultbuilding')
-engine.addEntity(defaultbuilding)
+const DAOScene = new Entity('DAOScene')
+engine.addEntity(DAOScene)
 const transform4 = new Transform({
   position: new Vector3(32,0,24),
   rotation: Quaternion.Euler(0, 90, 0),
   scale: new Vector3(1, 1, 1)
 })
-defaultbuilding.addComponentOrReplace(transform4)
-const gltfShape2 = new GLTFShape("models/DAOHouse.gltf")
-gltfShape2.withCollisions = true
-gltfShape2.isPointerBlocker = true
-gltfShape2.visible = true
-defaultbuilding.addComponentOrReplace(gltfShape2)
-// hud.attachToEntity(defaultbuilding)
+DAOScene.addComponentOrReplace(transform4)
+const DAOSceneShape = new GLTFShape("models/DAOHouse.gltf")
+DAOSceneShape.withCollisions = true
+DAOSceneShape.isPointerBlocker = true
+DAOSceneShape.visible = true
+DAOScene.addComponentOrReplace(DAOSceneShape)
+
+const DAOStage = new Entity('DAOStage')
+engine.addEntity(DAOStage)
+const transform3 = new Transform({
+  position: new Vector3(32,30,24),
+  rotation: Quaternion.Euler(0, 90, 0),
+  scale: new Vector3(1, 1, 1)
+})
+DAOStage.addComponentOrReplace(transform3)
+const DAOStageShape = new GLTFShape("models/DAOStage.gltf")
+DAOStageShape.withCollisions = true
+DAOStageShape.isPointerBlocker = true
+DAOStageShape.visible = true
+DAOStage.addComponentOrReplace(DAOStageShape)
+// hud.attachToEntity(DAOScene)
+
