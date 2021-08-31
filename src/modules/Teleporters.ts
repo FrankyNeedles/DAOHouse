541801dfa85b33a564c5a58fb276783ef4c6072e
twaldorf <trevor.automated@gmail.com>
2021-08-31 13:40:44 -0700

    add teleporters

---
+++ src/modules/Teleporters.ts
@@ -0,0 +1,41 @@
+import * as utils from '@dcl/ecs-scene-utils'
+import { movePlayerTo } from '@decentraland/RestrictedActions'
+
+export const setupTeleporters = (locationArray:Array<Vector3>):Array<Entity> => {
+
+    const entities = locationArray.map(location => {
+       const box = new Entity()
+  
+       //create shape for entity and disable its collision
+       box.addComponent(new BoxShape())
+       box.getComponent(BoxShape).withCollisions = false
+       box.getComponent(BoxShape).visible = false
+   
+       //set transform component with initial position
+       box.addComponent(new Transform({ position: location, scale: new Vector3(3,4,3), rotation: new Quaternion(0,0,.6,0) }))
+   
+       // create trigger area object, setting size and relative position
+       let triggerBox = new utils.TriggerBoxShape(new Vector3(3,4,3))
+   
+       //create trigger for entity
+       box.addComponent(
+         new utils.TriggerComponent(
+           triggerBox, //shape
+         {
+           onCameraEnter :() => {
+             movePlayerTo(new Vector3(40, 32, 25))
+           }
+         
+         }
+           
+         )
+       )
+   
+       //add entity to engine
+       engine.addEntity(box)
+       return box
+    })
+  
+    return entities
+  
+  }
